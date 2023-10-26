2599fd69111211e1e16b8865c17e4b7f8c78df57
Dhyanesh Naik <dhyaneshnaik111@gmail.com>
2023-10-26 18:27:18 +0100

    modofied urls.py main

--- dublintiffinservice/urls.py
+++ dublintiffinservice/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('customuser.urls')),
     path('bookings/', include('bookings.urls')),
     path('meals/', include('meals.urls')),
-    path('', include('customuser.urls'))
 ]
