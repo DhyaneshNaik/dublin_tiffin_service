c17154dc861e154fe94d00e735a4c9c18715f678
Dhyanesh Naik <dhyaneshnaik111@gmail.com>
2023-12-11 15:57:32 +0000

    removed code Vulnerabilities

--- dublintiffinservice/settings.py
+++ dublintiffinservice/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-32ev)mspj^l##@hejl2ih#3+*11!v(s*i71nka!wirhy$$ep73'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
